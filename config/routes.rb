49838fb23287659ee249fd6cb569da8980483f49
JunhoYoon95 <enffl95@icloud.com>
2022-06-29 12:08:12 +0900

    일단 devise 추가했고, 그에 따라서 해당 부분도 custom html 을 만들어 보려고 합니다

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  # TODO:: 해당 페이지는 전체 url에 /admin을 붙일 것인가요
   root to: "main#home"
   get '/home', to: 'main#home'
+  get '/users', to: 'user#index'
   
   
 end
