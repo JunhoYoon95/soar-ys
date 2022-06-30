e26514abc6592dc630b44fd788ae071123728420
JunhoYoon95 <enffl95@icloud.com>
2022-06-30 17:15:07 +0900

    style.bundle.css 에서 .header 에서 left:265px 을 없앴습니다

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,11 +3,12 @@ class UserController < ApplicationController
     end
     def parent_detail
         @id = params[:id]
-        # 유저의 role 을 판단하는 코드를 둡시다
         # 유저가 parent 라면  몇 명의 학생을 데이고 있는지에 대해서도 가져와야 합니다
-        # pranet 라면 student 들을 전부 데려오고, 데려온 학생의 이름들을 string 값으로 합칩니다
+        # student 들을 전부 데려오고, 데려온 학생의 이름들을 string 값으로 합칩니다
     end
     def teacher_detail
+        # 선생님 정보를 그대로 가져옵니다
+        @id = params[:id]
     end
     def student_detail
     end
