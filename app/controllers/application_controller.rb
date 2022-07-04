2d5ef62267185d077dab3acf9f41517ef811cfbf
JunhoYoon95 <enffl95@icloud.com>
2022-07-04 13:34:56 +0900

    rails devise create user 하는 경우 additional field 를 넣는 방법을 알았습니다. 해당 방법으로 name 필드를 가입시에 넣고 있습니다.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :init
+  before_action :configure_permitted_parameters, if: :devise_controller?
+ 
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
   
   def init
     @app_company = "회사명"
@@ -8,9 +14,6 @@ class ApplicationController < ActionController::Base
     @app_logo = ""
     @app_description = ""
     @app_url = ""
-
   end
-  
-  
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers =>  {user: 'configure_permitted_parameters'}
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   # TODO:: 해당 페이지는 전체 url에 /admin을 붙일 것인가요
