2c92a75ef77680720fe98f96d712a06ed854d5ae
JunhoYoon95 <enffl95@icloud.com>
2022-07-06 14:13:16 +0900

    teacher db column 헷갈리는 것들은 빼고 전부 넣어보았습니다

--- db/migrate/20220705070517_create_teachers.rb
+++ db/migrate/20220705070517_create_teachers.rb
@@ -1,6 +1,18 @@
 class CreateTeachers < ActiveRecord::Migration[5.0]
   def change
     create_table :teachers do |t|
+      t.string :uid
+      t.number :sex
+      t.number :location
+      t.string :video_link
+      t.boolean :video
+      t.boolean :interview
+      t.number :empathy
+      t.number :book_comprehension
+      t.number :logical
+      t.string :notes
+      t.string :education
+      t.string :major
 
       t.timestamps
     end
